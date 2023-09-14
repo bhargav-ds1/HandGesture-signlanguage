9a37366e39249d87684c7193103fc45a4363772f
Bhargav Vankayalapati <bhargav.vankayalapati@tu-dortmund.de>
2023-09-14 16:56:18 +0200

    Initial commit with the hand sign detection for GSL alphabets, moving from local to github

---
+++ InferFromVideo.py
@@ -0,0 +1,78 @@
+import pickle
+
+import cv2
+import mediapipe as mp
+import numpy as np
+
+model_dict = pickle.load(open('./model.p', 'rb'))
+model = model_dict['model']
+# Modify the index of the camera as per your necessity
+cap = cv2.VideoCapture(0)
+
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+
+hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
+
+letters=["A","B","C","D","E","F","G","H","I","K","L","M","N" ,"O" ,"P" ,"Q" ,"R" ,"SCH","T","U" ,"V" ,"W" ,"X" ,"Y" ,"S"]
+indices=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
+labels_dict = {k:v for k,v in zip(indices,letters)}
+has_extra = False
+counter=0
+while True:
+
+    data_aux = []
+    x_ = []
+    y_ = []
+
+    ret, frame = cap.read()
+
+    H, W, _ = frame.shape
+
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    results = hands.process(frame_rgb)
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(
+                frame,  # Frame of the Image
+                hand_landmarks,  # Detected Hand landmarks
+                mp_hands.HAND_CONNECTIONS,  # Detected Hand connections
+                mp_drawing_styles.get_default_hand_landmarks_style(),
+                mp_drawing_styles.get_default_hand_connections_style())
+
+        for hand_landmarks in results.multi_hand_landmarks:
+            for i in range(len(hand_landmarks.landmark)):
+                x = hand_landmarks.landmark[i].x
+                y = hand_landmarks.landmark[i].y
+
+                x_.append(x)
+                y_.append(y)
+
+            for i in range(len(hand_landmarks.landmark)):
+                x = hand_landmarks.landmark[i].x
+                y = hand_landmarks.landmark[i].y
+                data_aux.append(x - min(x_))
+                data_aux.append(y - min(y_))
+
+        x1 = int(min(x_) * W) - 10
+        y1 = int(min(y_) * H) - 10
+
+        x2 = int(max(x_) * W) - 10
+        y2 = int(max(y_) * H) - 10
+        if (len(data_aux)!=42):
+            continue
+        prediction = model.predict([np.asarray(data_aux)])
+
+        predicted_character = labels_dict[int(prediction[0])]
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 0), 4)
+        cv2.putText(frame, predicted_character, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 0), 3,
+                    cv2.LINE_AA)
+
+    cv2.imshow('frame', frame)
+    cv2.waitKey(1)
+
+
+cap.release()
+cv2.destroyAllWindows()
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# HandGesture-signlanguage
-Detection of Alphabets presented in German Sign language. 
+# HandSignDetection-signlanguage
+Detection of Alphabets presented in German Sign language.
+
+
+
+How to Run:
+
+1. Clone the repository
+2. Install the requirements using the requirements.txt
+2. Run InferFromVideo.py (modify the index of the camera, uses camera at index 0 by default)
+3. Use your right hand to show designated hand signs for the alphabets as per German sign language (GSL)
+4. See the results of the detection on top of the square surrounding the hand.
+
